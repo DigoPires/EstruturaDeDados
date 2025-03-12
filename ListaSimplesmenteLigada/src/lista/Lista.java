a6033c01090d6be80cb121803e0d284640e51923
DigoPires <rodrigo_pires2806@outlook.com>
2025-03-11 21:51:12 -0300

    Aula 11-03-2025

--- ListaSimplesmenteLigada/src/lista/Lista.java
+++ ListaSimplesmenteLigada/src/lista/Lista.java
@@ -1,5 +1,7 @@
 package lista;
 
+import modelo.Carro;
+
 public class Lista {
 
     private No inicio, atual, aux;
@@ -22,5 +24,63 @@ public class Lista {
             System.out.println(ref.getDados());
             ref = ref.getProx();
         }
+    }
+
+    /**
+     * Realizar a pesquisa linear na lista.
+     * @param id int
+     * @return No
+     */
+
+    public Carro pesquisar(int id){
+        No ref = inicio;
+        Carro carro;
+        while(ref!=null){
+            carro = (Carro)ref.getDados(); // Cating - Conversão temporária
+            if(id==carro.getId()){
+                return carro;
+            }
+            ref=ref.getProx();
+        }
+
+        return null;
+    }
+
+    public boolean remover(int id){
+        No ref = inicio;
+        No refAnterior=inicio;  
+        Carro carro;
+        while(ref!=null){
+            carro = (Carro)ref.getDados(); // Cating - Conversão temporária
+            if(id==carro.getId()){
+                break;
+            }
+            refAnterior = ref;
+            ref = ref.getProx();
+        }
+
+        if(ref!=null){
+            //remover
+            if(ref==inicio){ // Remove o primeiro Nó
+                inicio = inicio.getProx();
+                ref.setProx(null);
+            }
+            else if(ref==atual){ // Remove o último Nó
+                aux = refAnterior;
+                atual = aux;
+                aux.setProx(null);
+            }
+            else{
+                refAnterior.setProx(ref.getProx());
+                ref.setProx(null);
+            }
+            return true;
+        }
+
+        return false;
+    }
+
+    public void atualizar(int id){
+        
     }
 }
--- ListaSimplesmenteLigada/src/principal/UsaLista.java
+++ ListaSimplesmenteLigada/src/principal/UsaLista.java
@@ -8,7 +8,30 @@ public class UsaLista {
         Lista lista = new Lista();
         lista.inserir(new Carro("Fox", 2014));
         lista.inserir(new Carro("Jetta", 2015));
+        lista.inserir(new Carro("Nivus", 2022));
+        lista.inserir(new Carro("Polo", 2023));
 
         lista.exibir();
+        
+        // boolean teste = lista.remover(3);
+        // System.out.println("\n==== Removendo ====");
+        // if(teste){
+        //     lista.exibir();
+        // }
+        // else{
+        //     System.out.println("ID não encontrado!");
+        // }
+
+        Carro carro = lista.pesquisar(1);
+
+        System.out.println("\n==== Pesquisa ====");
+        if(carro != null){
+            System.out.println(carro);
+        }
+        else{
+            System.out.println("Carro não encontrado!");
+        }
+
+        // Método Atualizar - atualizar o ano do carro
     }
 }
